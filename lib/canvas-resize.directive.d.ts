259fa9a556948fcf391f5dfe84b1a614cd09b4d4
enelife <68290896+enelife@users.noreply.github.com>
2020-07-14 21:31:04 +0200

    Update canvas-resize.directive.d.ts

--- lib/canvas-resize.directive.d.ts
+++ lib/canvas-resize.directive.d.ts
@@ -4,7 +4,7 @@ export declare class CanvasResizeDirective implements AfterViewInit {
     private renderer;
     private signaturePad;
     private debounceTime?;
-    constructor(canvasElementRef: ElementRef<HTMLCanvasElement>, renderer: Renderer2);
+    constructor(canvasElementRef: ElementRef, renderer: Renderer2);
     ngAfterViewInit(): void;
     private resizeCanvas;
 }
